1faff0f69560e847edef9a1a554f0b50f07fb206
SUCHMOKUO <suchmokuo@gmail.com>
2019-11-14 10:58:24 +0800

    add declaration file for typescript

---
+++ index.d.ts
@@ -0,0 +1,69 @@
+type TaskFunc = (param: any) => any;
+
+declare namespace StaticPool {
+  type ConstructorParam = {
+    /** number of workers */
+    size: number;
+
+    /** path of worker file or a worker function */
+    task: string | TaskFunc;
+
+    /** data to pass into workers */
+    workerData?: any;
+  }
+}
+
+/**
+ * Threads pool with static task.
+ */
+declare class StaticPool {
+  constructor(param: StaticPool.ConstructorParam);
+
+  /**
+   * choose a idle worker to run the task
+   * with param provided.
+   */
+  exec(param: any): Promise<any>;
+
+  /**
+   * destroy this pool and terminate all threads.
+   */
+  destroy();
+}
+
+declare namespace DynamicPool {
+  type ExecParam = {
+    /** function to be executed. */
+    task: TaskFunc;
+
+    /** data to pass into workers. */
+    workerData?: any;
+  }
+}
+
+/**
+ * Threads pool that can run different function
+ * each call.
+ */
+declare class DynamicPool {
+  /**
+   * @param size number of threads.
+   */
+  constructor(size: number);
+
+  /**
+   * choose a idle worker to execute the function
+   * with context provided.
+   */
+  exec(param: DynamicPool.ExecParam): Promise<any>;
+
+  /**
+   * destroy this pool and terminate all threads.
+   */
+  destroy();
+}
+
+export { 
+  DynamicPool,
+  StaticPool
+};
